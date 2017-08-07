65f596762e6e1c486cf5ff7585f60270106ebe98
veyndan <veyndan@gmail.com>
2017-08-07 20:42:43 +0100

    Display text from txt intent

--- app/src/main/java/com/veyndan/texteditor/MainActivity.kt
+++ app/src/main/java/com/veyndan/texteditor/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.veyndan.texteditor
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import okio.Okio
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (intent.data != null) {
+            val textView = findViewById<TextView>(R.id.text)
+
+            val source = Okio.buffer(Okio.source(contentResolver.openInputStream(intent.data)))
+
+            textView.text = source.readUtf8()
+        }
     }
 }
